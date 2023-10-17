03be8ddac6876fafcee43956e2f00a3030efc805
Bruno Costa <brunogomesc@outlook.com.br>
2023-10-16 22:46:41 -0300

    finalizado implementacao de autorizacao

---
+++ PoderFinanceiro/Server/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoderFinanceiro.Shared;
+using PoderFinanceiro.Shared.Models;
+
+namespace PoderFinanceiro.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+
+            SessionDTO session = new();
+
+            if(login == null) throw new ArgumentNullException(nameof(login));
+            else if(login.Email == "admin@gmail.com" && login.Password == "admin")
+            {
+                session.Name = "Bruno Costa";
+                session.Email = login.Email;
+                session.Function = "Administrator";
+            }
+            else
+            {
+                session.Name = "Bruno Costa";
+                session.Email = login.Email;
+                session.Function = "Client"; 
+            }
+
+            return StatusCode(StatusCodes.Status200OK, session);
+
+        }
+
+    }
+}
---
+++ PoderFinanceiro/Shared/Models/SessionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoderFinanceiro.Shared.Models
+{
+    public class SessionDTO
+    {
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Function { get; set; }
+
+    }
+}
