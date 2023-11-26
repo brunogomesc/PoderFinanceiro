25a95857e3bd59fb3bc7f9a78c3fd54284085803
Bruno Costa <brunogomesc@outlook.com.br>
2023-11-26 20:59:50 -0300

    criacao earning model

--- PoderFinanceiro/Model/Earning.cs
+++ PoderFinanceiro/Model/Earning.cs
@@ -9,6 +9,25 @@ namespace PoderFinanceiro.Model
     public class Earning
     {
 
+        public Guid Id { get; set; }
+
+        public Guid IdCategory { get; set; }
+
+        public Guid IdSubCategory { get; set; }
+
+        public string TypeEarning { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime DateEarning { get; set; }
+
+        public string Observation { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
 
     }
 }
